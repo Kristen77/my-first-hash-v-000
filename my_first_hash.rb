c36c3cf2ba1e8c58c8da2553aab772e92fa3d82e
Kristen Robertson <kristen77@gmail.com>
2018-10-01 11:31:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,14 +21,11 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-shipping_manifest["oil paintings"]
-
-
 shipping_manifest["muskets"] = 2 
 
 shipping_manifest["gun powder"] = 4 
 
-  # return the shipping_manifest hash below
+
 
 end
+
